9b8700fbe33d3fbeb74ad207f0276e202093f87a
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-04-21 04:50:19 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,18 +7,30 @@ const content = document.querySelector(".content");
 
 const html = `
   <header class="header">
-    <div class="header__logo">
-      <img src="../images/logo.svg" alt="Logo" class="logo" />
-    </div>
+        <div class="navbar">
+          <div class="logo">
+            <img src="../images/logo.svg" alt="Logo" class="logo" />
+          </div>
 
-    <div class="header__text">
-      <h1 class="heading">
-      	 WE ARE CREATIVE
-      </h1>
-      <img
-        src="../images/icon-arrow-down.svg"
-        alt="arrow donw icon"
-        class="arrow"/>
-  </header>
+          <nav class="menu">
+            <ul>
+              <li><a href="about">About</a></li>
+              <li><a href="services">Services</a></li>
+              <li><a href="projects">Projects</a></li>
+              <li><a href="contact">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+
+        <div class="header__text">
+          <h1 class="heading">
+           WE ARE CREATIVE
+          </h1>
+          <img
+            src="../images/icon-arrow-down.svg"
+            alt="arrow donw icon"
+            class="arrow"
+          />
+      </header>
 `;
-content.insertAdjacentHTML("afterbegin", html);
+content.insertAdjacentHTML("beforeend", html);
